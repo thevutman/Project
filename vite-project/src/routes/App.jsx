f48bff52c08597051711056ea7a77755260f3372
CarelessDude <dhystymortal@gmail.com>
2024-01-08 16:34:51 -0500

    Santigod

---
+++ vite-project/src/routes/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import '../App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <p>Hola Santiago</p>
+  )
+}
+
+export default App
